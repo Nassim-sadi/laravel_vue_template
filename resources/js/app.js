1f5930868bf6bf32e3787b2900f5ca8fc05a1d48
nassim-sadi <nacimbreeze@gmail.com>
2024-05-05 16:32:58 +0100

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,9 @@
+import "./bootstrap";
+import { createApp } from "vue";
+import router from "./utils/router";
+import App from "./App.vue";
+
+const app = createApp(App);
+
+app.use(router);
+app.mount("#app");
---
+++ resources/js/pages/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h2>sup home</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    setup() {
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped></style>
